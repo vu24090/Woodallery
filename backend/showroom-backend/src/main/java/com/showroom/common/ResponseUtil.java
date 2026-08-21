b999c68c1e586bfe5f2a095ae51e8eee58b23591
cvtv2409 <binfaded147@gmail.com>
2026-08-21 22:43:01 +0700

    update

--- backend/showroom-backend/src/main/java/com/showroom/common/ResponseUtil.java
+++ backend/showroom-backend/src/main/java/com/showroom/common/ResponseUtil.java
@@ -5,13 +5,23 @@ import java.util.Map;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 public class ResponseUtil {
+
+    private final static String origin = "http://localhost:5173";
+
     private ResponseUtil() {
     }
 
     public static APIGatewayProxyResponseEvent jsonify(int statusCode, String body) {
         return new APIGatewayProxyResponseEvent()
                 .withStatusCode(statusCode)
-                .withHeaders(Map.of("Content-Type", "application/json"))
+                .withHeaders(Map.of(
+                    "Content-Type", "application/json",
+                    "Access-Control-Allow-Origin", origin,
+                    "Access-Control-Allow-Headers",
+                    "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent",
+                    "Access-Control-Allow-Methods",
+                    "OPTIONS,GET,POST,PUT,DELETE"
+            ))
                 .withBody(body);
     }
 }
